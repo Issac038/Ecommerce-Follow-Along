d5564103852a4d5af73c3f4ba5f148cd2c66cfad
Issac038 <issac.antony@kalvium.community>
2024-12-05 16:06:07 +0530

    created user model,user controller and configured multer

---
+++ backend/src/model/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {type:String,require:[true,'Please Enter the Nmae']},
+    email: {
+        type:String,
+        require:[true,'Please Enter Email'],
+        unique:[true,'Please Enter Unique Email Address']
+    },
+    password: {type:String,require:[true,'Please Enter Password']},
+    address: [
+        {city:String},
+        {country:String},
+        {add1:String},
+        {add2:String},
+        {zipcCode:String},
+        {addressType:String},
+    ],
+    role: {type:String,require:true},
+    avatar: {
+        url:{type:String,require:true},
+        public_id:{type:String,require:true},
+    },
+    resetPasswordToken: String,
+    resetPasswordTime: Date,
+    },
+    {versionKey:false}
+);
+
+const UserModel = mongoose.model('User',userSchema)
+module.exports = UserModel
