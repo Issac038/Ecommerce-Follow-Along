3aaf09fb48a0e50df97c7c2a60455daaa65a12aa
Issac038 <issac.antony@kalvium.community>
2025-01-04 12:02:01 +0530

    Milestone 10 completed

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -1,31 +1,36 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    name: {type:String,require:[true,'Please Enter the Nmae']},
+const userSchema = new mongoose.Schema(
+  {
+    Name: { type: String, required: [true, 'Please Enter the Name...'] },
     email: {
-        type:String,
-        require:[true,'Please Enter Email'],
-        unique:[true,'Please Enter Unique Email Address']
+      type: String,
+      required: [true, 'Please Enter Email.. '],
+      unique: [true, 'Please enter Unique Email Address'],
     },
-    password: {type:String,require:[true,'Please Enter Password']},
+    password: {
+      type: String,
+      required: [true, 'Please enter the password...'],
+    },
     address: [
-        {city:String},
-        {country:String},
-        {add1:String},
-        {add2:String},
-        {zipcCode:String},
-        {addressType:String},
+      { city: String },
+      { country: String },
+      { add1: String },
+      { add2: String },
+      { zipCode: String },
+      { addressType: String },
     ],
-    role: {type:String,require:true},
+    role: { type: String, default: 'user' },
     avatar: {
-        url:{type:String},
-        public_id:{type:String},
+      url: { type: String, required: true },
+      public_id: { type: String, required: true },
     },
-    resetPasswordToken: String,
+    resetPaswordToken: String,
     resetPasswordTime: Date,
-    },
-    {versionKey:false}
+  },
+  { versionKey: false }
 );
 
-const UserModel = mongoose.model('User',userSchema)
-module.exports = UserModel
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
